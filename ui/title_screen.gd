158372c3d83a556c2bbcdb7762f7f8d4f594e4c1
Daniel <craklyn@gmail.com>
2026-08-16 15:10:40 -0700

    Fix Input Bleed from title screen transitions

--- ui/title_screen.gd
+++ ui/title_screen.gd
@@ -1,16 +1,20 @@
 extends Control
 
-func _ready() -> void:
-	pass
-
-func _unhandled_input(event: InputEvent) -> void:
+func _gui_input(event: InputEvent) -> void:
 	if event is InputEventMouseButton and event.pressed and event.button_index == MOUSE_BUTTON_LEFT:
+		accept_event()
 		start_game()
 	elif event is InputEventScreenTouch and event.pressed:
+		accept_event()
 		start_game()
 	elif event.is_action_pressed("ui_accept"):
+		accept_event()
 		start_game()
 
 func start_game() -> void:
+	print("Starting game...")
+	InputManager.has_click = false
 	AudioManager.play_sfx("click")
-	get_tree().change_scene_to_file("res://main.tscn")
+	var err = get_tree().change_scene_to_file("res://main.tscn")
+	if err != OK:
+		print("Failed to change scene! Error code: ", err)
